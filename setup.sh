1591b716ca6a7e52fe3c672dcb0844055e973071
Tommy Leeman <leeman.tommy@gmail.com>
2015-10-22 14:16:16 +0000

    added installer.conf (incomplete) and setup.sh (incomplete)

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+########################################################
+
+release=jessie				# debian release
+mirror=						# debian mirror to use
+imagesize=5					# image size in mb
+bootsize=2					# boot partition size in mb
+
+########################################################
+set -e
+
+function mk_image {
+	IMG=debian-$release-ua-netinst-`date +%Y%m%d`.img
+	rm -f $IMG
+	rm -f $IMG.bz2
+	rm -f $IMG.xz
+
+	bootsize="+$bootsize"M
+
+	dd if=/dev/zero of=$IMG bs=1M count=$imagesize
+
+# scripted. do not indent!
+fdisk $IMG <<EOF
+n
+p
+1
+
+$bootsize
+t
+b
+n
+p
+2
+
+
+w
+EOF
+# end script
+}
+
+function mk_fs {
+	kpartx -as $IMG
+	mkfs.vfat /dev/mapper/loop0p1
+	mkfs.ext4 /dev/mapper/loop0p2
+	kpartx -d $IMG
+}
